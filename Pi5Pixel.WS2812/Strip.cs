ce86349303f827c892a824c4bc4d649b43745d87
Philipp Mondl <philipp.mondl.pm@gmail.com>
2025-01-01 02:56:25 +0100

    pixels array accessible outside of Strip.cs

--- Pi5Pixel.WS2812/Strip.cs
+++ Pi5Pixel.WS2812/Strip.cs
@@ -4,7 +4,7 @@ public class Strip
 {
     public int LedCount { get; }
 
-    private readonly Color[] _pixels;
+    public Color[] Pixels { get; }
     
     private readonly WS2812Driver _driver;
 
@@ -12,24 +12,24 @@ public class Strip
     {
         _driver = driver;
         LedCount = _driver.GetLedCount();
-        _pixels = new Color[LedCount].Select(_ => new Color()).ToArray();
+        Pixels = new Color[LedCount].Select(_ => new Color()).ToArray();
     }
 
-    public void SetPixelColor(int index, byte red, byte green, byte blue) => _pixels[index] = new Color(red, green, blue);
+    public void SetPixelColor(int index, byte red, byte green, byte blue) => Pixels[index] = new Color(red, green, blue);
     
-    public void SetPixelColor(int index, Color color) => _pixels[index] = color;
+    public void SetPixelColor(int index, Color color) => Pixels[index] = color;
     
     public void SetAllPixels(byte red, byte green, byte blue) => SetAllPixels(new Color(red, green, blue));
     
     public void SetAllPixels(Color color)
     {
-        for (var i = 0; i < _pixels.Length; i++) _pixels[i] = color;
+        for (var i = 0; i < Pixels.Length; i++) Pixels[i] = color;
     }
 
     public void Show()
     {
-        var buffer = new List<byte>(_pixels.Length * 3);
-        foreach (var pixel in _pixels)
+        var buffer = new List<byte>(Pixels.Length * 3);
+        foreach (var pixel in Pixels)
         {
             buffer.Add(pixel.Green);
             buffer.Add(pixel.Red);
